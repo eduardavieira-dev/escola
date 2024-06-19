550f3144202cb10a94064d332416bc7331f86fc2
Eduarda Vieira <159597766+eduardavieira-dev@users.noreply.github.com>
2024-06-19 10:17:00 -0300

    Update insert.php

--- php/insert.php
+++ php/insert.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // verifica se a inserção foi bem-sucedida
     if ($inserir) {
         // Redireciona para a página de login após o cadastro
-        header("Location: ./login.html");
+        header("Location: ../login.html");
         exit();
     } else {
         echo "Erro ao cadastrar aluno: " . mysqli_error($connection);
